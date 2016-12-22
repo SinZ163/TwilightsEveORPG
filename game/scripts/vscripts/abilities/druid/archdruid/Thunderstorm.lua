2c33c99c682cce54e40ff8373bd1f400963aef9f
AlexSem98 <nscorpionul@yahoo.com>
2016-12-22 11:08:19 +0200

    Added Twister and initial draft of Thunderstorm - Arch Druid (#4)
    Added Twister and progress on Thunderstorm ability - Arch Druid

--- game/scripts/vscripts/abilities/druid/archdruid/Thunderstorm.lua
+++ game/scripts/vscripts/abilities/druid/archdruid/Thunderstorm.lua
@@ -1,7 +1,12 @@
 teve_druid_archdruid_thunderstorm = class({})
+LinkLuaModifier( "modifier_teve_druid_archdruid_thunderstorm_thinker", "abilities/druid/archdruid/modifier_teve_druid_archdruid_thunderstorm_thinker", LUA_MODIFIER_MOTION_NONE )
 
 function teve_druid_archdruid_thunderstorm:OnSpellStart()
     print("Thunderstorm from Lua!")
+    local kv = {
+        duration = 1.2 --Fact check plz
+    }
+    CreateModifierThinker( self:GetCaster(), self, "modifier_teve_druid_archdruid_thunderstorm_thinker", kv, self:GetCursorPosition(), self:GetCaster():GetTeamNumber(), false )
 end
 
 function teve_druid_archdruid_thunderstorm:GetAOERadius()
@@ -10,4 +15,13 @@ end
 
 function teve_druid_archdruid_thunderstorm:GetCastAnimation()
     return ACT_DOTA_CAST_ABILITY_4
+end
+
+function teve_druid_archdruid_thunderstorm:GetManaCost(level)
+
+    if level == -1 then
+        level = self:GetLevel()
+    end
+
+    return 50 + (25 * level )
 end
--- game/scripts/vscripts/abilities/druid/archdruid/Twister.lua
+++ game/scripts/vscripts/abilities/druid/archdruid/Twister.lua
@@ -2,8 +2,75 @@ teve_druid_archdruid_twister = class({})
 
 function teve_druid_archdruid_twister:OnSpellStart()
     print("twister from Lua!")
+    self.archdruid_twister_speed = 1000
+    self.archdruid_twister_width_initial = 150
+    self.archdruid_twister_width_end = 200
+    self.archdruid_twister_distance = 600
+    self.archdruid_twister_damage = 100 * self:GetLevel()
+
+   
+
+  
+    --Do we want unit targetting??? nope
+    local vPos = nil
+    if self:GetCursorTarget() then
+        vPos = self:GetCursorTarget():GetOrigin()
+    else
+        vPos = self:GetCursorPosition() 
+    end
+
+    local vDirection = vPos - self:GetCaster():GetOrigin()
+    vDirection.z = 0.0
+    vDirection = vDirection:Normalized()
+
+    self.archdruid_twister_speed = self.archdruid_twister_speed * ( self.archdruid_twister_distance / ( self.archdruid_twister_distance - self.archdruid_twister_width_initial ) )
+
+    local info = {
+        EffectName = "particles/units/heroes/hero_invoker/invoker_tornado.vpcf",
+        Ability = self,
+        vSpawnOrigin = self:GetCaster():GetOrigin(),
+        fStartRadius = self.archdruid_twister_width_initial,
+        fEndRadius = self.archdruid_twister_width_end,
+        vVelocity = vDirection * self.archdruid_twister_speed,
+        fDistance = self.archdruid_twister_distance,
+        Source = self:GetCaster(),
+        iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+        iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+    }
+
+    ProjectileManager:CreateLinearProjectile ( info )
+    
+    EmitSoundOn ( "Hero_Invoker.Tornado.Cast", self:GetCaster() )
 end
 
+function teve_druid_archdruid_twister:OnProjectileHit ( hTarget, vLocation )
+    if hTarget ~= nil and ( not hTarget:IsMagicImmune() ) and ( not hTarget:IsInvulnerable() ) then
+        local damage = {
+            victim = hTarget,
+            attacker = self:GetCaster(),
+            damage = self.archdruid_twister_damage,
+            damage_type = DAMAGE_TYPE_MAGICAL,
+            ability = self
+        }    
+
+        ApplyDamage( damage )
+
+        local vDirection = vLocation - self:GetCaster():GetOrigin()
+        vDirection.z = 0.0
+        vDirection = vDirection:Normalized()
+    end
+
+    return false
+end        
+
+function teve_druid_archdruid_twister:GetManaCost(level)
+    if level == -1 then
+        level = self:GetLevel()
+    end
+  
+    return 60 + (20 * level )
+end 
+  
 function teve_druid_archdruid_twister:GetCastAnimation()
     return ACT_DOTA_CAST_ABILITY_1
 end
