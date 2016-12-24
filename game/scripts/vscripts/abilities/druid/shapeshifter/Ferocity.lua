dcfcd6d79033186e7a45880be5f0abb38680f1c4
DESKTOP-VSTBS5M\nscor <nscorpionul@yahoo.com>
2016-12-24 18:46:02 +0200

    Added Ferocity and Lycanthophy passive abilities for Shapeshifter

---
+++ game/scripts/vscripts/abilities/druid/shapeshifter/Ferocity.lua
@@ -0,0 +1,7 @@
+teve_druid_shapeshifter_ferocity = class({})
+
+LinkLuaModifier("modifier_teve_druid_shapeshifter_ferocity", "abilities/druid/shapeshifter/modifier_teve_druid_shapeshifter_ferocity.lua", LUA_MODIFIER_MOTION_NONE)
+
+function teve_druid_shapeshifter_ferocity:GetIntrinsicModifierName()
+    return 'modifier_teve_druid_shapeshifter_ferocity'
+end
