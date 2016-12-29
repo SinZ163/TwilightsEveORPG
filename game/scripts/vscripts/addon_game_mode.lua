20b6c83b64217becb12a0a0693bde6cf71754715
Samuel Cattini-Schultz <catts16@gmail.com>
2016-12-29 18:53:01 +1100

    Indentation fix

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -45,10 +45,10 @@ end
 function Precache( context )
     --[[
         Precache things we know we'll use.  Possible file types include (but not limited to):
-            PrecacheResource( "model", "*.vmdl", context )
-            PrecacheResource( "soundfile", "*.vsndevts", context )
-            PrecacheResource( "particle", "*.vpcf", context )
-            PrecacheResource( "particle_folder", "particles/folder", context )
+        PrecacheResource( "model", "*.vmdl", context )
+        PrecacheResource( "soundfile", "*.vsndevts", context )
+        PrecacheResource( "particle", "*.vpcf", context )
+        PrecacheResource( "particle_folder", "particles/folder", context )
     ]]
 end
 
@@ -58,28 +58,28 @@ function Activate()
     GameRules.TwilightsEveORPG:InitGameMode()
 end
 function TwilightsEveORPG:InitGameMode()
-        local levels = {}
-        for i = 1, 300 do
-                levels[i] = getExpForLevel(i)
-        end
+    local levels = {}
+    for i = 1, 300 do
+        levels[i] = getExpForLevel(i)
+    end
     GameRules:GetGameModeEntity():SetCustomXPRequiredToReachNextLevel(levels)
-        GameRules:GetGameModeEntity():SetUseCustomHeroLevels(true)
-        ListenToGameEvent("npc_spawned", Dynamic_Wrap(TwilightsEveORPG, "OnNPCSpawned"), self)
-        ListenToGameEvent("dota_player_gained_level", Dynamic_Wrap(TwilightsEveORPG, "OnPlayerGainedLevel"), self)
+    GameRules:GetGameModeEntity():SetUseCustomHeroLevels(true)
+    ListenToGameEvent("npc_spawned", Dynamic_Wrap(TwilightsEveORPG, "OnNPCSpawned"), self)
+    ListenToGameEvent("dota_player_gained_level", Dynamic_Wrap(TwilightsEveORPG, "OnPlayerGainedLevel"), self)
         
 end
 function TwilightsEveORPG:OnNPCSpawned(keys)
-        local npc = EntIndexToHScript(keys.entindex)
+    local npc = EntIndexToHScript(keys.entindex)
 
-        -- when a hero first spawns
-        if npc:IsRealHero() and npc.bFirstSpawned == nil then
-                npc:AddItemByName("item_ring_of_protection")
-                npc:AddItemByName("item_ring_of_protection")
-                npc:AddItemByName("item_ring_of_protection")
-                npc:SwapItems(DOTA_ITEM_SLOT_1, DOTA_ITEM_SLOT_7)
-                npc:SwapItems(DOTA_ITEM_SLOT_2, DOTA_ITEM_SLOT_8)
-                npc:SwapItems(DOTA_ITEM_SLOT_3, DOTA_ITEM_SLOT_9)
-        end
+    -- when a hero first spawns
+    if npc:IsRealHero() and npc.bFirstSpawned == nil then
+        npc:AddItemByName("item_ring_of_protection")
+        npc:AddItemByName("item_ring_of_protection")
+        npc:AddItemByName("item_ring_of_protection")
+        npc:SwapItems(DOTA_ITEM_SLOT_1, DOTA_ITEM_SLOT_7)
+        npc:SwapItems(DOTA_ITEM_SLOT_2, DOTA_ITEM_SLOT_8)
+        npc:SwapItems(DOTA_ITEM_SLOT_3, DOTA_ITEM_SLOT_9)
+    end
 end
 function TwilightsEveORPG:OnPlayerGainedLevel( event )
     local player = EntIndexToHScript( event.player )
@@ -88,32 +88,32 @@ function TwilightsEveORPG:OnPlayerGainedLevel( event )
         local currentCount = 0
         local maxCount = 0
         for i = 0, 5 do
-                local ability = hero:GetAbilityByIndex(i)
-                if (ability ~= nil) then
-                        currentCount = currentCount + ability:GetLevel()
-                        maxCount = maxCount + ability:GetMaxLevel()
-                end
+            local ability = hero:GetAbilityByIndex(i)
+            if (ability ~= nil) then
+                currentCount = currentCount + ability:GetLevel()
+                maxCount = maxCount + ability:GetMaxLevel()
+            end
         end
         if maxCount > hero:GetLevel() then
-                maxCount = hero:GetLevel()
+            maxCount = hero:GetLevel()
         end
         hero:SetAbilityPoints(maxCount - currentCount)
 end
 
 function getExpForLevel(level)
-        if level <= 1 then
-                return 0
-        elseif level == 2 then
-                return 100
-        elseif level == 3 then
-                return 450
-        else
-                local base = 450
-                local exp = 450
-                for i = 0, level - 3 do
-                        exp = exp + (base+100*i)
-                end
-
-                return exp
+    if level <= 1 then
+        return 0
+    elseif level == 2 then
+        return 100
+    elseif level == 3 then
+        return 450
+    else
+        local base = 450
+        local exp = 450
+        for i = 0, level - 3 do
+            exp = exp + (base+100*i)
         end
+
+        return exp
+    end
 end
